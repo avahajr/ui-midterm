c6bd226261dab1a7056fb7556cbf3226001bf70f
Ava Hajratwala <100488972+avahajr@users.noreply.github.com>
2024-03-05 19:48:13 -0500

    update with input validation

--- main/static/farmer.js
+++ main/static/farmer.js
@@ -32,58 +32,108 @@ function isValidSearchTerm(search_term) {
     return true
 }
 
+async function validateEntry() {
+    $(".warning").empty();
 
-function validateEntry() {
     let checkboxes = $('input[name="days"]:checked')
     let selectedDays = checkboxes.map(function () {
         return this.value;
     }).get();
-    let newEntry = {
-        market_name: $("#market_name").val(),
-        borough: $("#borough").val(),
-        image: $("#image").val(),
-        street_address: $("#street_address").val(),
-        latitude: 0,
-        longitude: 0,
-        days: selectedDays,
-        vendors_list: $("#vendors").val().split(", "),
-        year_round: $('input[name="year_round"]:checked').val(),
-        summary: $("#summary").val()
-    };
-    let errors = {};
-    console.log(newEntry)
 
-    for (let prop in newEntry) {
-        if (newEntry.hasOwnProperty(prop)) {
-            let value = newEntry[prop];
-            let dataType = typeof value;
+    // check if the address is valid & retrieve lat, lng
+    let zip = $("#zip").val();
+    let street_address = $("#street_address").val()
+    let [lat, lng] = [null, null]
+    const apiKey = "AhCY625KQxZJyShmnHRlfjMuPw_dKcblIGFwahIKbuSn1iy0zkMHaLi0Ic9YbUzH";
+    // call to validate the address & geolocate
 
-            // Validate data types
-            switch (prop) {
-                case 'latitude':
-                case 'longitude':
-                    // Validate numeric values for latitude and longitude
-                    if (dataType !== 'number') {
-                        errors[prop] = 'Should be a numeric value.';
-                    }
-                    break;
-                case 'year_round':
-                    // Validate boolean value for year_round
-                    if (!(newEntry["year_round"] === 'true' || newEntry['year_round'] === 'false')) {
-                        errors[prop] = 'Should be a boolean value.';
-                    }
-                    break;
-                // Add additional cases for other properties if needed
+    let isValid = true;
+
+    // validation
+    let summary = $("#summary").val()
+    if (summary.trim() === '') {
+        $(`#summary_warning`).append("Field cannot be empty.")
+        $("#summary").focus().empty();
+        isValid = false
+    }
+
+    let vendors = $("#vendors_list").val()
+    if (vendors.trim() === '') {
+        $(`#vendors_list_warning`).append("Field cannot be empty.")
+        $("#vendors_list").focus().empty();
+        isValid = false
+    }
+    let year_round = $('input[name="year_round"]:checked').val();
+    if (year_round !== "true" && year_round !== "false") {
+        $("#yearRoundWarning").append("Please select a value.");
+        $("#year_round_true").focus()
+        isValid = false
+    }
+    if (selectedDays.length === 0) {
+        $('#daysWarning').append("Market must have at least one day.")
+        $("#day_sunday").focus()
+        isValid = false
+    }
+
+    let image = $("#image").val()
+    if (image.trim() === '') {
+        $(`label[for=image].warning`).append("Field cannot be empty.");
+        $('#image').focus().empty();
+        isValid = false
+
+    }
+    if (street_address.trim() === '') {
+        $("label[for=street_address].warning").append("Field cannot be empty.")
+        $('#street_address').focus().empty();
+        isValid = false
+    }
+    if (isNaN(Number(zip))) {
+        $("#zipWarning").append("Zipcode must be a number");
+        $('#zip').focus();
+        isValid = false
+    } else if (isValid) {
+
+        await $.ajax({
+                url: `http://dev.virtualearth.net/REST/v1/Locations?key=${apiKey}&locality=New York City&adminDistrict=NY&addressLine=${street_address}&postalCode=${zip}`,
+                type: "GET",
+                dataType: 'json',
+                success: function (data) {
+                    [lat, lng] = data['resourceSets'][0]['resources'][0]['point']['coordinates']
+                },
+                error: function (e) {
+                    console.error("Error:", e)
+                }
             }
+        )
+        if (lat === null) {
+            $("label[for=street_address].warning").append("Could not find matching address.")
+            $('#street_address').focus();
+            isValid = false
         }
     }
 
-    // Output errors or do something with them
-    if (Object.keys(errors).length > 0) {
-        console.error('Validation errors:', errors);
-        // Handle errors as needed
-    } else {
-        console.log('Validation successful. Do something with the entry:', newEntry);
+    let market_name = $("#market_name").val();
+    if (market_name.trim() === '') {
+        $(`label[for=market_name].warning`).append("Field cannot be empty.");
+        $("#market_name").focus().val("");
+        isValid = false
+    }
+
+    if (isValid) {
+        let newEntry = {
+            market_name: market_name,
+            borough: $("#borough").val(),
+            image: image,
+            street_address: street_address,
+            zip: Number(zip),
+            latitude: Number(lat),
+            longitude: Number(lng),
+            days: selectedDays,
+            vendors_list: vendors.split(", "),
+            year_round: year_round,
+            summary: summary,
+        };
+        console.log('Validation successful. Adding entry:', newEntry);
         $.ajax({
             url: '/add',
             type: "POST",
@@ -92,13 +142,14 @@ function validateEntry() {
             success: function (data) {
                 console.log("successfully submitted", data)
                 // clear all data fields
-
-
+                $("#add_market")[0].reset();
             },
             error: function (e) {
                 console.error("while input was valid, there was an error adding new market:", e)
             }
         });
+    } else {
+        console.error('something was wrong with the input.')
     }
 }
 
@@ -116,9 +167,8 @@ $(document).ready(function () {
         doSearch(searchTerm);
     })
 
-    $("#submit-form").click(function(e) {
+    $("#submit-form").click(function (e) {
         e.preventDefault();
-        console.log("submit")
         validateEntry();
     })
 
